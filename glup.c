75a468379e12871a8b058a21a69b79241589b972
Rafael Aziz Da Silva Rocha <rafaelazizlvr@gmail.com>
2017-09-28 01:16:10 -0300

    first commit add copyrights

---
+++ glup.c
@@ -0,0 +1,29 @@
+/* ************************************************************************ *
+ *    Glup, v0.01                                                           *
+ *    Drinking game that consists in random commands to the players         *
+ *                                                                          *
+ *    Copyright (C) 2017 by Rafael Aziz Da Silva Rocha                      *
+ *                                                                          *
+ *    This program is free software; you can redistribute it and/or modify  *
+ *    it under the terms of the GNU General Public License as published by  *
+ *    the Free Software Foundation; either version 2 of the License, or     *
+ *    (at your option) any later version.                                   *
+ *                                                                          *
+ *    This program is distributed in the hope that it will be useful,       *
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *    GNU General Public License for more details.                          *
+ *                                                                          *
+ *    You should have received a copy of the GNU General Public License     *
+ *    along with this program; if not, write to the                         *
+ *    Free Software Foundation, Inc.,                                       *
+ *    59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ *                                                                          *
+ *    To contact the author, please write to:                               *
+ *    Rafael Aziz Da Silva Rocha <rafaelazizlvr@gmail.com>                  *
+ *    Webpage: http://www.upe.br                                            *
+ *    Phone: +55 (81) 99940-3183                                            *
+ * ************************************************************************ *
+ * 
+ */
+
